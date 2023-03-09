828569ecbf6876c9c0f83e39a04b617a7b616dae
anbee123 <anhipchipchip@gmail.com>
2023-03-09 23:00:56 +0100

    add username field

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,13 +6,18 @@ const Login = () => {
     e.preventDefault()
     console.log(e.target.email.value)
     console.log(e.target.password.value)
+    console.log(e.target.username.value)
     const email = e.target.email.value
     const password = e.target.password.value
+    const username = e.target.username.value
     if (!email) {
       setErrorText('Email is required')
       return
     } else if (!password) {
       setErrorText('Password is required')
+      return
+    } else if (!username) {
+      setErrorText('User Name is required')
       return
     }
   }
@@ -20,6 +25,10 @@ const Login = () => {
     <div>
       <h1>Login</h1>
       <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">User Name</label>
+          <input type='text' name="username" placeholder="User Name" />
+        </div>
         <div>
           <label htmlFor="email">Email</label>
           <input type='email' name="email" placeholder="Email" />
