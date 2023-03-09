cbbb41a58237625ff5d99927cf69b9a438f21fe1
anbee123 <anhipchipchip@gmail.com>
2023-03-09 22:49:33 +0100

    update signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,37 @@
+import { useState } from 'react'
+
 const Signup = () => {
+  const [errorText, setErrorText] = useState()
+  const handleLogin = (e) => {
+    e.preventDefault()
+    console.log(e.target.email.value)
+    console.log(e.target.password.value)
+    const email = e.target.email.value
+    const password = e.target.password.value
+    if (!email) {
+      setErrorText('Email is required')
+      return
+    } else if (!password) {
+      setErrorText('Password is required')
+      return
+    }
+  }
   return (
     <div>
-      this is Signup
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type='email' name="email" placeholder="Input email here" />
+        </div>
+        <div>
+          <label htmlFor="password">Email</label>
+          <input type='password' name="password" placeholder="Input password here" />
+        </div>
+        <div>
+          {errorText}
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
