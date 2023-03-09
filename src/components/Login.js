05f21261115c75d3d3f76d928189ac015d841f48
anbee123 <anhipchipchip@gmail.com>
2023-03-09 22:47:00 +0100

    show error message for invalid input

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,20 @@
+import { useState } from 'react'
+
 const Login = () => {
+  const [errorText, setErrorText] = useState()
   const handleLogin = (e) => {
     e.preventDefault()
     console.log(e.target.email.value)
     console.log(e.target.password.value)
+    const email = e.target.email.value
+    const password = e.target.password.value
+    if (!email) {
+      setErrorText('Email is required')
+      return
+    } else if (!password) {
+      setErrorText('Password is required')
+      return
+    }
   }
   return (
     <div>
@@ -14,6 +26,9 @@ const Login = () => {
         <div>
           <label htmlFor="password">Email</label>
           <input type='password' name="password" placeholder="Input password here" />
+        </div>
+        <div>
+          {errorText}
         </div>
         <button type="submit">Login</button>
       </form>
