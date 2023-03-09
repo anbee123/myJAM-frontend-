1354da70f2345259693e375d90b57adc50ce85c8
anbee123 <anhipchipchip@gmail.com>
2023-03-09 22:18:20 +0100

    test fetch data from api

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/Home'
 import LoginPage from './components/Login'
+import { useEffect } from 'react';
+import { testApi } from './apis/auth'
 
 function App() {
+  useEffect(() => {
+    const funcAPI = async () => {
+      try {
+        const data = await testApi()
+        console.log({data})
+      } catch (err) {
+        console.log(err.response.data)
+      }
+    }
+    funcAPI()
+  }, [])
   return (
     <div className="App">
       <Routes>
