e718268730a89a586e2df8664645a82d6036f2ba
anbee123 <anhipchipchip@gmail.com>
2023-03-09 23:26:00 +0100

    remove email field on login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,20 +4,15 @@ const Login = () => {
   const [errorText, setErrorText] = useState()
   const handleLogin = (e) => {
     e.preventDefault()
-    console.log(e.target.email.value)
     console.log(e.target.password.value)
     console.log(e.target.username.value)
-    const email = e.target.email.value
     const password = e.target.password.value
     const username = e.target.username.value
-    if (!email) {
-      setErrorText('Email is required')
+    if (!username) {
+      setErrorText('User Name is required')
       return
     } else if (!password) {
       setErrorText('Password is required')
-      return
-    } else if (!username) {
-      setErrorText('User Name is required')
       return
     }
   }
@@ -28,10 +23,6 @@ const Login = () => {
         <div>
           <label htmlFor="username">User Name</label>
           <input type='text' name="username" placeholder="User Name" />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type='email' name="email" placeholder="Email" />
         </div>
         <div>
           <label htmlFor="password">Password</label>
