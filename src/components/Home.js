f9025f04ed0755839d54fdab5195798ae2aaa6e9
anbee123 <anhipchipchip@gmail.com>
2023-03-10 00:04:01 +0100

    fetch music api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,23 @@
 import * as Auth from '../apis/auth'
+import * as MusicApi from '../apis/music'
+import { useEffect } from 'react'
 
 const Home = () => {
   const currentUser = Auth.getCurrentUser()
   console.log({currentUser})
+
+  useEffect(() => {
+    const testAPI = async () => {
+      try {
+        const res = await MusicApi.fetchMusicData()
+        console.log({res})
+      } catch (err) {
+        console.log('error: ', err.response.data)
+      }
+    }
+    testAPI()
+  }, [])
+
   return (
     !currentUser ?
     <div>
