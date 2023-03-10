357df1341edefcd7d3e82d67a6ee182c5814e555
anbee123 <anhipchipchip@gmail.com>
2023-03-10 01:01:55 +0100

    add Music list component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,18 @@
 import * as Auth from '../apis/auth'
 import * as MusicApi from '../apis/music'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import MusicList from './MusicList'
 
 const Home = () => {
   const currentUser = Auth.getCurrentUser()
+  const [musics, setMusics] = useState([])
   console.log({currentUser})
 
   useEffect(() => {
     const testAPI = async () => {
       try {
         const res = await MusicApi.fetchMusicData()
+        if (res.length > 0) setMusics(res)
         console.log({res})
       } catch (err) {
         console.log('error: ', err.response.data)
@@ -28,6 +31,7 @@ const Home = () => {
     <div>
       this is Home Page
       Logged in
+      <MusicList />
     </div>
   )
 }
---
+++ src/components/MusicList.js
@@ -0,0 +1,8 @@
+const MusicList = () => {
+  return (
+    <div>
+      This is music list
+    </div>
+  )
+}
+export default MusicList
