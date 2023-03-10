d6d9c44bd4ec3bb44f87cd7d1057f93b659ee924
anbee123 <anhipchipchip@gmail.com>
2023-03-10 01:30:33 +0100

    update MusicItem

--- src/components/MusicItem.js
+++ src/components/MusicItem.js
@@ -1,9 +1,18 @@
+import styled from "styled-components"
 
 const MusicItem = ({musicItem}) => {
   return (
-    <div>
+    <ItemContainer>
       {musicItem.title}
-    </div>
+    </ItemContainer>
   )
 }
 export default MusicItem
+
+const ItemContainer = styled.div`
+  display: flex;
+  padding: 0.5rem;
+  margin: 0.2rem;
+  border: 1px solid rgba(0,0,0,0.2);
+  border-radius: 0.5rem;
+`
