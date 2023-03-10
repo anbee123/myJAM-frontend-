50fd9d40694b9ea0940ce945b995ba5799eed92b
anbee123 <anhipchipchip@gmail.com>
2023-03-10 01:26:58 +0100

    added styled-components

--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -1,13 +1,21 @@
+import styled from "styled-components"
 import MusicItem from "./MusicItem"
 
 const MusicList = ({musics}) => {
   return (
     <div>
-      This is music list
-      {musics.map(item => (
-        <MusicItem musicItem={item} />
-      ))}
+      <h2>This is music list</h2>
+      <ListContainer>
+        {musics.map(item => (
+          <MusicItem musicItem={item} />
+        ))}
+      </ListContainer>
     </div>
   )
 }
 export default MusicList
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
