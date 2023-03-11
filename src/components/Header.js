f4c7cfd6b485277e3b02ea923e00b8fb1b66f095
anbee123 <anhipchipchip@gmail.com>
2023-03-11 02:45:48 +0100

    added Logo on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
+import styled from "styled-components"
+import Logo from '../assets/logo.jpeg'
+
 const Header = () => {
   return (
-    <div>
+    <HeaderContainer>
+      <LogoImage src={Logo} alt='JAM Logo' />
       <h1>This is Header</h1>
-    </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </HeaderContainer>
   )
 }
 export default Header
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+const LogoImage = styled.img`
+  height: 40px;
+`
