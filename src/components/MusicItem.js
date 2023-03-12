0af47db14209cb749c26a552a78a1fabb3515cb5
anbee123 <anhipchipchip@gmail.com>
2023-03-12 17:57:06 +0100

    update Music Item background

--- src/components/MusicItem.js
+++ src/components/MusicItem.js
@@ -22,6 +22,7 @@ const CardContainer = styled(Link)`
   margin: 0.2rem;
   border: 1px solid rgba(0,0,0,0.2);
   border-radius: 0.5rem;
+  background: rgba(255, 255, 255, 0.4);
   h5 {
     margin: 0;
   }
