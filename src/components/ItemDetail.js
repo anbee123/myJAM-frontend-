bef037b44c0b5c0da58da5bb6aa4069753382860
anbee123 <anhipchipchip@gmail.com>
2023-03-14 01:38:43 +0100

    apply tailwindcss on ItemDetail component

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom"
-import styled from "styled-components"
 import { useAppContext } from "../context"
 import ReactAudioPlayer from 'react-audio-player'
 
@@ -20,56 +19,21 @@ const ItemDetail = () => {
     return date.toISOString().substring(11, 19)
   }
   return (
-    <ItemDetailContainer>
-      <ItemImage>
-        <Image src={musicItem.thumb} alt={musicItem.title} />
+    <div className="w-4/5 flex m-auto p-8">
+      <div className="w-2/5 h-fit min-w-[320px] border-neutral-600 bg-slate-300 flex flex-col items-center mr-12 p-2 rounded-3xl border-2 border-solid">
+        <img className="w-full mb-2 rounded-3xl" src={musicItem.thumb} alt={musicItem.title} />
         <ReactAudioPlayer src={musicItem.stream_url} controls />
-      </ItemImage>
-      <Details>
-        <h2>{musicItem.title}</h2>
-        <DetailText>
-          <p><b>Genre</b> : {musicItem.genre}</p>
-          <p><b>Duration</b> : {formateTime(musicItem.duration)}</p>
-          <p><b>Release Date</b> : {musicItem.release_date}</p>
-        </DetailText>
+      </div>
+      <div className="flex flex-col">
+        <h2 className="text-2xl font-bold my-7">{musicItem.title}</h2>
+        <div className="flex justify-between flex-wrap text-left">
+          <p className="my-2"><b>Genre</b> : {musicItem.genre}</p>
+          <p className="my-2"><b>Duration</b> : {formateTime(musicItem.duration)}</p>
+          <p className="my-2"><b>Release Date</b> : {musicItem.release_date}</p>
+        </div>
         <p>{musicItem.description}</p>
-      </Details>
-    </ItemDetailContainer>
+      </div>
+    </div>
   )
 }
 export default ItemDetail
-
-const ItemDetailContainer = styled.div`
-  width: 80%;
-  margin: auto;
-  display: flex;
-  padding: 2rem;
-`
-const ItemImage = styled.div`
-  width: 40%;
-  height: fit-content;
-  min-width: 320px;
-  margin-right: 3rem;
-  border-radius: 20px;
-  border: 2px solid #525252;
-  padding: 0.5rem;
-  background-color: #d5d5d5;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-const Image = styled.img`
-  width: 100%;
-  border-radius: 20px;
-  margin-bottom: 0.5rem;
-`
-const Details = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-const DetailText = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  text-align: left;
-`
